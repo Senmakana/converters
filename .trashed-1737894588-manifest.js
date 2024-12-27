668a35ae2f04d92e3cdb2621a1ba383e87449f6c
Senmakana <123734431+Senmakana@users.noreply.github.com>
2024-12-27 15:33:39 +0300

    Add .trashed-1737894588-manifest.js

---
+++ .trashed-1737894588-manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Safaricom",
+    "short_name": "Safaricom",
+    "description": "Safaricom phone number input app",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#5da9c9",
+    "icons": [
+        {
+            "src": "icon.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "icon.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
